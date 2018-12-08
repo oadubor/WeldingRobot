86adb8e6a134faf55c89085fd5cc6ecaeabd4c3e
Obi Adubor <oadubor@gmail.com>
2018-12-08 10:50:03 -0500

    Delete gravity_comp_testing.m

--- gravity_comp_testing.m
+++
@@ -1,8 +0,0 @@
-function [] = gravity_comp_testing()
-    clear all;
-    robot = Robot3D();        
-    control = RobotControl3D(robot, 'Robot B');
-    control.fix_hebilookup();
-    control.gains();
-    control.gravity_compensation();
-end
